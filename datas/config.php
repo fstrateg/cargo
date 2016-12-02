48049a8d7231a1ee6a6ec59ce3a141ddad1e5894
fstrateg <Alexeym78@mail.ru>
2016-12-02 13:37:37 +0600

    Транспорт (готово)

--- datas/config.php
+++ datas/config.php
@@ -17,7 +17,7 @@ $cfg['mysqlhost'] = 'localhost';	// Database host URL
 $cfg['mysqlport'] = '';				// Database port, if non-default
 $cfg['mysqluser'] = 'root';			// Database user
 $cfg['mysqlpassword'] = '';			// Database password
-$cfg['mysqldb'] = 'cargo';		// Database name
+$cfg['mysqldb'] = 'bulat';		// Database name
 // MySQL database charset and collate. Very useful when MySQL server uses different charset rather than site
 // See the list of valid values here: http://dev.mysql.com/doc/refman/5.1/en/charset-charsets.html
 $cfg['mysqlcharset'] = 'utf8';
@@ -27,7 +27,7 @@ $cfg['mysqlcollate'] = 'utf8_unicode_ci';
 // Main site URL without trailing slash.
 // ========================
 
-$cfg['mainurl'] = 'http://cargo.kz';
+$cfg['mainurl'] = 'http://cargo.git';
 $cfg['site_id'] = 'd71cf259bdb10129def5d17b86187a86';
 $cfg['secret_key'] = '6e9a0d178974680e0230058b4bf377e6';
 $cfg['multihost'] = FALSE;			// Allow multiple host names for this site
@@ -145,7 +145,7 @@ $cfg['shield_force'] = FALSE;
 // Else do not change.
 // ========================
 
-$db_x = 'gr_'; // Default: cot_, prefix for extra fields' table(s)
+$db_x = 'cg_'; // Default: cot_, prefix for extra fields' table(s)
 
 // Examples:
 // $db_auth			= 'my_custom_auth';
