87e3f45eb60477004c63344473edd3a680d5277c
fstrateg <Alexeym78@mail.ru>
2017-01-26 17:54:23 +0600

    Добавлен jivosite

---
+++ plugins/jivosite/jivosite.footer.php.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * [BEGIN_COT_EXT]
+ * Hooks=footer.tags
+ * [END_COT_EXT]
+ */
+
+defined('COT_CODE') or die('Wrong URL.');
+
+$out['footer_rc'] .= $cfg['plugin']['jivosite']['code'];
+
+?>
---
+++ plugins/jivosite/jivosite.setup.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * [BEGIN_COT_EXT]
+ * Code=jivosite
+ * Name=JivoSite
+ * Category=community-social
+ * Description=Подключает к сайту онлайн-консультанта JivoSite. 
+ * Version=1.0.0
+ * Date=
+ * Author=devkont
+ * Copyright=&copy; CMSWorks Team 2010 - 2013
+ * Notes=http://cmsworks.ru
+ * Auth_guests=R
+ * Lock_guests=12345A
+ * Auth_members=RW
+ * Lock_members=12345A
+ * [END_COT_EXT]
+ * 
+ * [BEGIN_COT_EXT_CONFIG]
+ * code=01:textarea:::Код онлайн-консультанта
+ * [END_COT_EXT_CONFIG]
+ */
+
+/**
+ * JivoSite Plugin
+ *
+ * @package jivosite
+ * @version 1.0.0
+ * @author devkont
+ * @copyright (c) CMSWorks Team 2010 - 2013
+ * @license BSD
+ */
+
+?>
