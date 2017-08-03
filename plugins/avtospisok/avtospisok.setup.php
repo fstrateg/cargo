b0a36ac28650851eb323fec204986a27a519ea21
kot903491 <kot903491@gmail.com>
2017-08-03 06:38:56 +0500

    новый список авто

---
+++ plugins/avtospisok/avtospisok.setup.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * [BEGIN_COT_EXT]
+ * Code=avtospisok
+ * Name=Список транспорта
+ * Description=Выводит список транспорта в любом месте сайта функцией create_my_selectbox($optname,$sel), работает администрирование списка
+ * Version=1.1.0
+ * Date=02.08.2017
+ * Author= Timur Ahmetzyanov
+ * Copyright=(c) Timurka)))
+ * Notes=
+ * Auth_guests=R
+ * Lock_guests=W12345A
+ * Auth_members=RW
+ * Lock_members=12345A
+ * [END_COT_EXT]
+
+ * [BEGIN_COT_EXT_CONFIG]
+ *
+ * [END_COT_EXT_CONFIG]
+ */
+defined ('COT_CODE') or die ('WRONG URL');
+?>
---
+++ plugins/avtospisok/inc/avtospisok.functions.php
@@ -0,0 +1,57 @@
+<?php
+function create_my_selectbox($optname,$sel){
+    if ($sel==null){
+        $sel=0;}
+    cot::$db->registerTable('spisok_avtotransport');
+    global $db, $db_spisok_avtotransport;
+    $result = '<select name=' . $optname . '>
+<optgroup label="">';
+    $res = $db->query("SELECT id,name FROM $db_spisok_avtotransport WHERE hot = TRUE")->fetchall();
+    foreach ($res as $item)
+    {
+        $id=$item['id'];
+        $name=$item['name'];
+        if($sel==$id){
+            $result=$result . "<option value=$id selected>$name</option>";}
+        else{$result=$result . "<option value=$id>$name</option>";}
+    }
+    $result = $result . '</optgroup>
+<optgroup label="--------">';
+    $res = $db->query("SELECT id,name FROM $db_spisok_avtotransport WHERE hot = FALSE  ORDER BY name")->fetchall();
+    foreach ($res as $item)
+    {
+        $id=$item['id'];
+        $name=$item['name'];
+        if($sel==$id){
+            $result=$result . "<option value=$id selected>$name</option>";}
+        else{$result=$result . "<option value=$id>$name</option>";}
+    }
+    $result = $result . '</optgroup></select>';
+    return $result;
+}
+
+
+function create_admin()
+{
+    cot::$db->registerTable('spisok_avtotransport');
+    global $db, $db_spisok_avtotransport;
+    $result = "";
+    $res = $db->query("SELECT * FROM $db_spisok_avtotransport")->fetchall();
+    foreach ($res as $item)
+    {
+        $result=$result . '<tr>';
+        $id=$item['id'];
+        $name=$item['name'];
+        $result = $result . "<td class='name'>$name</td>";
+        $hot = $item['hot'];
+        if($hot)
+        {$ch='selected';}
+        else{$ch='';}
+        $result=$result . "<td class='hot'><select name='hot[$id]'>
+        <option value='0' $ch>0</option><option value='1' $ch>1</option>
+        </select></td><td><input name='del[$id]' type='checkbox'></td>";
+
+        $result=$result . "</tr>";
+    }
+    return $result;
+}
