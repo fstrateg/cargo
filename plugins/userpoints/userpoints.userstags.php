2b9df41e42529102fcd56feee4d2525c2352e39c
fstrateg <Alexeym78@mail.ru>
2017-11-14 17:57:55 +0600

    Работа над рейтинговой системой в профиле (картинки)

--- plugins/userpoints/userpoints.userstags.php
+++ plugins/userpoints/userpoints.userstags.php
@@ -20,8 +20,22 @@ if(is_array($user_data)){
     $p=(int)$user_data['user_userpoints'];
 	$temp_array['USERPOINTS'] = number_format($p, '1', '.', ' ');
     $img='stars1.png';
-    if ($p>10) $img='stars2.png';
-    if ($p>20) $img='stars3.png';
-    if ($p>40) $img='stars4.png';
-    $temp_array['USERSTARS']='<img src="/images/icons/default/'.$img.'" />';
+    if ($p<=50)
+    {
+        $temp_array['USERSTARS']='';
+        return;
+    }
+    if ($p>50) $img='11';
+    if ($p>99) $img='12';
+    if ($p>199) $img='13';
+    if ($p>299) $img='14';
+    if ($p>399) $img='15';
+    if ($p>499) $img='21';
+    if ($p>499) $img='21';
+    if ($p>999) $img='22';
+    if ($p>1999) $img='23';
+    if ($p>4999) $img='24';
+    if ($p>9999) $img='25';
+    if ($p>19999) $img='31';
+    $temp_array['USERSTARS']='<img src="/images/rating/'.$img.'-s.gif" />';
 }
